9797506cbdd1e7cfd95cc20e7a55ed219bbfbf57
Lars-Andreas Nystad <lan@dips.no>
2018-06-13 04:46:41 +0100

    insert befoer move

---
+++ SendingResultClient/Viewmodels/ChooseStevneViewModel.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Practices.Prism.Commands;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Input;
+
+namespace SendingResultClient.Viewmodels
+{
+    public class ChooseStevneViewModel : ViewModelBase
+    {
+       
+            private string m_selectedPath;
+
+            private DelegateCommand m_openFileDialogCommand;
+
+            public ICommand OpenFileDialogCommand
+            {
+                get
+                {
+                    if (m_openFileDialogCommand == null)
+                    {
+                        m_openFileDialogCommand = new DelegateCommand(this.OpenFileDialogExecute);
+                    }
+
+                    return m_openFileDialogCommand;
+                }
+            }
+
+            public string SelectedPath
+            {
+                get
+                {
+                    return this.m_selectedPath;
+                }
+                set
+                {
+                    this.m_selectedPath = value;
+                    this.OnPropertyChanged("SelectedPath");
+                }
+            }
+
+            public void OpenFileDialogExecute()
+            {
+                using (var dlg = new System.Windows.Forms.FolderBrowserDialog())
+                {
+                    if (!string.IsNullOrWhiteSpace(SelectedPath) && Directory.Exists(SelectedPath))
+                    {
+                        dlg.SelectedPath = SelectedPath;
+                    }
+
+                    var result = dlg.ShowDialog();
+                    if (result == DialogResult.OK)
+                    {
+                        SelectedPath = dlg.SelectedPath;
+                    }
+                }
+            }
+        }
+    
+}
---
+++ SendingResultClient/Viewmodels/MainViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SendingResultClient.Viewmodels
+{
+    public class MainViewModel : ViewModelBase
+    {
+
+        public MainViewModel()
+        {
+            m_chooseStevneViewModel = new ChooseStevneViewModel();
+        }
+
+        private ChooseStevneViewModel m_chooseStevneViewModel;
+
+       public ChooseStevneViewModel ChooseStevneViewModel
+        {
+            get
+            {
+                return this.m_chooseStevneViewModel;
+            }
+            set
+            {
+                this.m_chooseStevneViewModel = value;
+                this.OnPropertyChanged("ChooseStevneViewModel");
+            }
+        }
+
+    }
+}
