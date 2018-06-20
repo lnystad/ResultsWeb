7e15769756d73a88251b0337a585ca32fcfc134f
Lars-Andreas Nystad <lan@dips.no>
2018-06-20 05:29:45 +0100

    stepped version

--- WebResults/Common/FileUploaderService/Properties/AssemblyInfo.cs
+++ WebResults/Common/FileUploaderService/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("FileUploaderService")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("DIPS ASA")]
+[assembly: AssemblyCompany("ROPLAN")]
 [assembly: AssemblyProduct("FileUploaderService")]
-[assembly: AssemblyCopyright("Copyright © DIPS ASA 2014")]
+[assembly: AssemblyCopyright("Copyright © ROPLAN 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
